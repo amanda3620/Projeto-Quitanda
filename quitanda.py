e8d2b164c8bc640d819bc28c6e3e0b49c8e8d0a2
Amanda <amanda.santos.senai@gmail.com>
2023-11-10 10:25:22 -0300

    new

--- quitanda.py
+++ quitanda.py
@@ -64,54 +64,6 @@ def acesso():
         return
 render_template("login.html", msg="Usuário/senha incorretos!")
 
-# Rota da página ADM
-@app.route("/adm")
-def adm():
-    if verifica_sessao():
-        iniciar_db()
-        conexao = conecta_database
-        produtos = conexao.execute('SELECT * FROM produtos ORDER BY id_prod DESC').fetchall()
-        conexao.close()
-        title = "Administração"
-        return render_template("adm.html", produtos=produtos, title=title)
-    else:
-        return redirect("/login")
-
-# Rota de logout
-@app.route("/logout")
-def logout():
-    global login 
-    login = False
-    session.clear()
-    return redirect("/")
-
-# Rota da página de cadastro
-@app.route("/cadprodutos")
-def cadprodutos():
-    if verifica_sessao():
-        title = "Cadastro de produtos"
-        return render_template("cadprodutos.html", title=title)
-    else:
-        return redirect("/login")
-
-# Rota da página de cadastro no banco
-@app.route("/cadastro", methods=['post'])
-def cadastro():
-    if verifica_sessao():
-        nome_prod=request.form['nome_prod']
-        desc_prod=request.form['desc_prod']
-        preco_prod=request.form['preco_prod']
-        img_prod=request.files['img_prod']
-        id_foto=str(uuid.uuid4().hex)
-        filename=id_foto+nome_prod+'.png'
-        img_prod.save("static/img/produtos/"+filename)
-        conexao = conecta_database()
-        conexao.execute('INSERT INTO produtos (nome_prod, desc_prod, preco_prod, img_prod) VALUES (?, ?, ?, ?)', (nome_prod, desc_prod, preco_prod, filename))
-        conexao.commit()
-        conexao.close()
-        return redirect("/adm")
-    else:
-        return redirect("/login")
 
 # Para finalizar a rota 
 app.run(debug=True)
